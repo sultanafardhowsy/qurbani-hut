4203758307c355e5fe6ce66a8e06d92c4e12f3de
sultanafardhowsy <sfardhowsy@gmail.com>
2026-05-02 13:55:45 +0600

    verceldeployment modified

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000" // Use your actual domain in production
+  baseURL: process.env.BETTER_AUTH_URL // Use your actual domain in production
 })
 
 // Export the helpers from the specific instance above
